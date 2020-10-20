c0d3e9cfbf36223a44c091f74c600a559f3c6546
thangbk2209 <thangbk2209@gmail.com>
2020-10-20 14:35:57 +0700

    no message

--- config.py
+++ config.py
@@ -183,11 +183,11 @@ class Config:
 
         METAHEURISTIC_METHOD = 'pso'  # pso, whale
 
-        FITNESS_TYPE = 'bayesian_autoscaling'  # validation_error, bayesian_autoscaling
+        FITNESS_TYPE = 'validation_error'  # validation_error, bayesian_autoscaling
         FITNESS_NORMALIZE_METHOD = 'normalized_value'  # 'smape', 'normalized_value'
         VALUE_OPTIMIZE = 'all_parameter'  # 'hyper_parameter', 'all_parameter'
 
-        VERBOSE = 0
+        VERBOSE = 2
         LEARNING_RATE = 3e-4
         MAX_ITER = 200
         NUM_PARTICLE = 30
